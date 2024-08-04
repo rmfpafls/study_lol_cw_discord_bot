88e3dff59590ddff72900ef5bc053bb5dddf25c0
rmfpafls <brown915@naver.com>
2024-08-05 01:35:31 +0900

    10명의 플레이어들의 점수를 저장하여 2팀으로 나눕니다.

--- lol_fight.py
+++ lol_fight.py
@@ -1,3 +1,5 @@
+import random
+
 # 티어별 점수
 tier_scores = {
     '챌': 31,
@@ -33,7 +35,6 @@ tier_scores = {
     '아4': 1,
 }
 
-
 def parse_input(input_str):
     # 입력 문자열을 공백과 쉼표로 분리하여 리스트로 변환
     participants = input_str.split(', ')
@@ -42,31 +43,51 @@ def parse_input(input_str):
     participant_scores = {}
 
     for participant in participants:
-        if '-' in participant:
             name, tier = participant.split('-')
             # 점수로 변환
             score = tier_scores.get(tier, 0)
             # 딕셔너리에 추가
             participant_scores[name] = score
-
+    # print(participant_scores)
+    # {'승재': 17, '찬일': 19, '진산': 18, '영훈': 9, '이건': 19, '준희': 14, '효중': 5, '재민': 11, '유빈': 5, '성우': 11}
     return participant_scores
 
 
 def sort_and_split_teams(participant_scores):
-    # 점수에 따라 참여자들을 오름차순으로 정렬
-    sorted_participants = sorted(participant_scores.items(), key=lambda item: item[1], reverse=True)
-
+    # 점수에 따라 참여자들을 내림순으로 정렬
+    sorted_participant = {k: v for k, v in sorted(participant_scores.items(), key=lambda item: item[1])}
+    # print(sorted_participant)
+    #{'효중': 5, '유빈': 5, '영훈': 9, '재민': 11, '성우': 11, '준희': 14, '승재': 17, '진산': 18, '찬일': 19, '이건': 19}
     blue_team = {}
     red_team = {}
 
-    n = len(sorted_participants)
+    # (1,10) (2,9) (3,7) (4,6) 5 5 로 나누기
+    for i in range(4):
 
-    # 팀 배정
-    for i, (name, score) in enumerate(sorted_participants):
-        if i % 2 == 0:  # 짝수 인덱스 (0, 2, 4, ...)는 블루팀
-            blue_team[name] = score
-        else:  # 홀수 인덱스 (1, 3, 5, ...)는 레드팀
-            red_team[name] = score
+
+
+    # # 참가자 목록을 리스트로 변환
+    # participants = list(sorted_participant.items())
+    # # print(participants)
+    # #[('효중', 5), ('유빈', 5), ('영훈', 9), ('재민', 11), ('성우', 11), ('준희', 14), ('승재', 17), ('진산', 18), ('찬일', 19), ('이건', 19)]
+    #
+    # # 위에서부터 5명은 블루-레드-블루-레드-블루
+    # for i in range(5):
+    #     if i % 2 == 0:
+    #         blue_team[participants[i][0]] = participants[i][1]
+    #     else:
+    #         red_team[participants[i][0]] = participants[i][1]
+    # print(blue_team, red_team)
+    #
+    # # 아래 5명은 레드-블루-레드-블루-레드
+    # for i in range(5, 10):
+    #     if i % 2 == 0:
+    #         red_team[participants[i][0]] = participants[i][1]
+    #     else:
+    #         blue_team[participants[i][0]] = participants[i][1]
+
+    print("블루팀:", blue_team)
+    print("레드팀:", red_team)
 
     return blue_team, red_team
 
